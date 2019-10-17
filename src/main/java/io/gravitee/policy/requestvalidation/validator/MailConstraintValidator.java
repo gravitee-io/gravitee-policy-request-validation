3ab4ae07e1ac1e259b6aacad18a5b2ca69394ace
Nicolas Géraud <nicolas.geraud@gmail.com>
2019-10-17 11:41:09 +0200

    fix(email): validate encoded emails
    fix gravitee-io/issues#2746

--- src/main/java/io/gravitee/policy/requestvalidation/validator/MailConstraintValidator.java
+++ src/main/java/io/gravitee/policy/requestvalidation/validator/MailConstraintValidator.java
@@ -25,7 +25,7 @@ import java.util.regex.Pattern;
 public class MailConstraintValidator extends PatternConstraintValidator {
 
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[A-Z0-9._%+-]+(@|%40)[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @Override
     public boolean isValid(String value) {
